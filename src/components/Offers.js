507b1c48cb1b14817e5a73bffa4527c3c4362193
MrMuzyk <mimuzyk@gmail.com>
2018-08-05 22:51:43 +0200

    almost finished main page

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -20,8 +20,6 @@ class Offers extends Component{
       }).then( resp => {
         return resp.json();
       }).then( data => {
-        console.log(data)
-        console.log('call')
           this.setState({
             offers: data,
             isLoading: false
@@ -34,19 +32,37 @@ class Offers extends Component{
     }
 
     render(){
-      console.log(this.state.offers);
       if (this.state.isLoading == false) {
-        return <div className="">
-          <h2>Oferty</h2>
+        return <div className="offers-section main-width">
+          <h2 className="offers-section-title">Oferty</h2>
           {
             this.state.offers.offers.map( (elem, index) => {
-              return <div>{elem.name}</div>
+              return <div key={elem.index} className="offers-section-offer-container">
+                <img src={elem.backgroundImagePath} className="offers-section-offer-image"/>
+                <div className="offers-section-offer-details">
+                  <h3 className="offers-section-offer-title">{elem.name}</h3>
+                  <p className="offers-section-offer-description">{elem.description}</p>
+                  <div className="offers-section-offer-container-buttons-container">
+                    <div className="offers-section-button offers-section-button-price">
+                      <p>{elem.price}</p>
+                    </div>
+                    <div className="offers-section-icons-container">
+                      <img src="/images/front-page/offers/calendar.svg" className="offers-section-icon"/>
+                      <img src="/images/front-page/offers/map-marker.svg" className="offers-section-icon"/>
+                      <img src="/images/front-page/offers/plane.svg" className="offers-section-icon"/>
+                      <img src="/images/front-page/offers/map.svg" className="offers-section-icon"/>
+                      <img src="/images/front-page/offers/price-tag.svg" className="offers-section-icon"/>
+                    </div>
+                    <button className="offers-section-button offers-section-button-details">Więcej ></button>
+                  </div>
+                </div>
+              </div>
             })
           }
         </div>
       }else {
         return <div className="">
-          <h2>Oferty</h2>
+          <h2 className="offers-section-title">Oferty</h2>
       </div>
       }
     }
