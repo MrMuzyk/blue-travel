b4ac4788c9ef7c77fb1049548691c2f886910285
karolczykgreg <grzegorz.karolczyk@tratif.com>
2018-08-10 20:25:18 +0200

    upgraded calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -4,6 +4,88 @@ import ReactDOM from 'react-dom';
 import Month from './Month';
 import './calendar.css';
 
+const WRZESIEN_2018 = (<div className="calendar__grid">
+  <div>Pn</div>
+  <div>Wt</div>
+  <div>Śr</div>
+  <div>Cz</div>
+  <div>Pt</div>
+  <div>So</div>
+  <div>Nd</div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div className="calendar__selected">7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+  <div>11</div>
+  <div>12</div>
+  <div>13</div>
+  <div>14</div>
+  <div>15</div>
+  <div>16</div>
+  <div>17</div>
+  <div>18</div>
+  <div>19</div>
+  <div>20</div>
+  <div>21</div>
+  <div>22</div>
+  <div>23</div>
+  <div>24</div>
+  <div>25</div>
+  <div>26</div>
+  <div>27</div>
+  <div>28</div>
+  <div>29</div>
+  <div>30</div>
+</div>);
+
+const PAZDZIERNIK_2018 = (<div className="calendar__grid">
+  <div>Pn</div>
+  <div>Wt</div>
+  <div>Śr</div>
+  <div>Cz</div>
+  <div>Pt</div>
+  <div>So</div>
+  <div>Nd</div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div className="calendar__selected">4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+  <div>11</div>
+  <div>12</div>
+  <div>13</div>
+  <div>14</div>
+  <div>15</div>
+  <div>16</div>
+  <div className="calendar__selected">17</div>
+  <div>18</div>
+  <div>19</div>
+  <div>20</div>
+  <div>21</div>
+  <div>22</div>
+  <div>23</div>
+  <div>24</div>
+  <div>25</div>
+  <div>26</div>
+  <div>27</div>
+  <div>28</div>
+  <div>29</div>
+  <div>30</div>
+</div>);
+
+
+
 class Calendar extends Component{
   rootElement = document.getElementById('root')
     render() {
@@ -14,10 +96,10 @@ class Calendar extends Component{
             <h1>Kalendarz</h1>
             <div className="calendar__month__container">
               <div className="calendar__month">
-                <Month name="sierpien"/>
+                <Month month={9} selectedDays={[5, 2]} lmao={WRZESIEN_2018}/>
               </div>
               <div className="calendar__month">
-                <Month name="wrzesien"/>
+                <Month month={10} selectedDays={[9, 13]}  lmao={PAZDZIERNIK_2018}/>
               </div>
             </div>
           </div>
--- src/components/Calendar/Month.js
+++ src/components/Calendar/Month.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
 import './month.css';
 
+const monthsNames = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień',
+'Maj', 'Czerwiec', 'Lipiec', 'Śierpień', 'Wrzesień',
+'Październik', 'Listopad', 'Grudzień'];
+const monthsDaysLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
 class Month extends Component{
-  rootElement = document.getElementById('root')
-    render() {
+  getClassName(day) {
+    for(let i=0; i<this.props.selectedDays.length; i++) {
+      if(day === this.props.selectedDays[i]){
+        return 'calendar__selected';
+      }
+      return '';
+    }
+  } 
+  generateGrid(){
+    let temp = [];
+    for(let i=0; i < monthsDaysLength[this.props.month]; i++){
+      temp = [...temp, <div className={this.getClassName(i+1)}>{i+1}</div>];
+    }
+    return temp;
+  }
+  render() {
+    let grid = this.generateGrid();
       return (
-        <div>{this.props.name}</div>
+      <div>
+        <div className="title">{monthsNames[this.props.month]} 2018</div>
+          {this.props.lmao}
+        </div>
       )
     }
 }
